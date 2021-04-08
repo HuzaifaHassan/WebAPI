35b43002a3fed030cd94376eeb8cd20d0b12b7aa
huzaifa hassan <huzaifahassan97@hotmail.com>
2021-04-08 12:23:10 +0500

    Get,Post,Put

--- WebAPI/Controllers/DepartmentController.cs
+++ WebAPI/Controllers/DepartmentController.cs
@@ -69,5 +69,28 @@ namespace WebAPI.Controllers
             }
             return new JsonResult("Successfully Added");
         }
+        [HttpPut]
+
+        public JsonResult Put(Department dep)
+        {
+            string query = @"update dbp.Department set DepartmentName='"+dep.DepartmentName+@"'
+                              where DepartmentId="+dep.DepartmentId+@"";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+
+            }
+            return new JsonResult("Successfully updated");
+        }
     }
 }
